ba0bb44f67e067e14fa0cfe106eeeb60118dc88b
mshamrz <m.moroz.pn@ucu.edu.ua>
2026-04-06 19:19:22 +0300

    Update solution.py for "Maximum F..."

--- Maximum Frequency Stack/solution.py
+++ Maximum Frequency Stack/solution.py
@@ -1,31 +1,45 @@
+class Node:
+    def __init__(self, data):
+        self._data = data
+        self._next = None
+    def get_data(self):
+        return self._data
+    def get_next(self):
+        return self._next
+    def set_next(self, node):
+        self._next = node
 class Stack:
     def __init__(self):
-        self.items = []
-
+        self._head = None
+        self._top = None
     def push(self, x):
-        self.items.append(x)
-
+        node = Node(x)
+        node.set_next(self._head)
+        self._head = node
+        self._top = x
     def pop(self):
-        return self.items.pop()
-
+        value = self._top
+        self._head = self._head.get_next()
+        self._top = self._head.get_data() if self._head else None
+        return value
     def top(self):
-        return self.items[-1]
-
+        return self._top
     def empty(self):
-        return len(self.items) == 0
-from collections import defaultdict
+        return self._head is None
+
 class FreqStack:
-
     def __init__(self):
-        self.freq = defaultdict(int)
-        self.group = defaultdict(Stack)
+        self.freq = {}
+        self.group = {}
         self.maxfreq = 0
-
     def push(self, val: int) -> None:
-        self.freq[val] += 1
-        self.group[self.freq[val]].push(val)
-        if self.freq[val] > self.maxfreq:
-            self.maxfreq = self.freq[val]
+        self.freq[val] = self.freq.get(val, 0) + 1
+        f = self.freq[val]
+        if f not in self.group:
+            self.group[f] = Stack()
+        self.group[f].push(val)
+        if f > self.maxfreq:
+            self.maxfreq = f
 
     def pop(self) -> int:
         val = self.group[self.maxfreq].pop()
